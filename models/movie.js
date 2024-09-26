f5b48c0a9bcc4edd85ff26574834d9336c71f827
ulaskarakas <ulaskarakas95@gmail.com>
2024-09-27 00:15:23 +0300

    models created for user & movie

---
+++ models/movie.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+
+const movieSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    releaseYear: {
+        type: Number,
+        required: true
+    },
+    genre: {
+        type: String,
+        required: true
+    },
+    director: {
+        type: String,
+        required: true
+    },
+    writer: {
+        type: String,
+        required: true
+    },
+    star: {
+        type: String,
+        required: true
+    },
+    duration: {
+        type: Number,
+        required: true
+    },
+    rating: {
+        type: Number,
+        required: true,
+        min: 0,
+        max: 10
+    }
+});
+
+module.exports = mongoose.model('Movie', movieSchema);
---
+++ models/user.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    username:{
+        type: String,
+        required: true,
+        unique: true // Eşsiz olmasını sağlar
+    },
+    name: { 
+        type: String, 
+        required: true // Zorunlu olmasını sağlar
+    },
+    email: { 
+        type: String, 
+        required: true, 
+        unique: true // Eşsiz olmasını sağlar
+    },
+    password: { 
+        type: String, 
+        required: true 
+    },
+    createdAt: { 
+        type: Date, 
+        default: Date.now, // Kullanıcı oluşturulduğunda tarih otomatik olarak atanır
+    },
+});
+
+module.exports = mongoose.model('User', userSchema);
